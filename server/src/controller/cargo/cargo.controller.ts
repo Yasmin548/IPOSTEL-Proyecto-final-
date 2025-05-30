8af6a00531a9d916f0c0673bf0781e07ac364b31
Moisarm <v.30247986.mr@gmail.com>
2025-05-29 21:12:12 -0400

    [ADD] Cargo list controller

---
+++ server/src/controller/cargo/cargo.controller.ts
@@ -0,0 +1,18 @@
+import { cargoListService } from "../../services/cargo.service";
+import { IFunctionResponse, TCargo } from "../../types/index.types";
+import { safe } from "../../wrapper/safe.wrapper";
+import { ICargoController } from "../interface/index.interface";
+
+
+export class cargoController implements ICargoController {
+    public async cargoListController(): Promise<IFunctionResponse<TCargo[] | null>> {
+        return safe(async()=>{
+            return await cargoListService()
+        },{
+            successStatus:200,
+            successMessage:"Cargos Obtenidos Correctamente"
+        })
+    }
+
+    
+}
--- server/src/controller/interface/index.interface.ts
+++ server/src/controller/interface/index.interface.ts
@@ -1,5 +1,10 @@
-import { IFunctionResponse, TEmpleado } from "../../types/index.types";
+import { IFunctionResponse, TCargo, TEmpleado } from "../../types/index.types";
 
 export interface IEmpleadoController{
     empleadoListController():Promise <IFunctionResponse<TEmpleado[] | null>>
+}
+
+export interface ICargoController{
+    cargoListController():Promise <IFunctionResponse<TCargo[] | null>>
+    createCargoController(cargo:TCargo):Promise<IFunctionResponse<TCargo>>
 }
