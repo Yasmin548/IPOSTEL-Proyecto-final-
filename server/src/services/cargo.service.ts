cc3b5700695fe39fc4de8a54b612f7e17c495454
Moisarm <v.30247986.mr@gmail.com>
2025-05-29 20:27:26 -0400

    [ADD] create cargo service

--- server/src/services/cargo.service.ts
+++ server/src/services/cargo.service.ts
@@ -1,7 +1,16 @@
 import Prisma from "../config/prisma.config"
+import { TCargo } from "../types/index.types"
 
 
 export const cargoListService = async () =>{
     const cargosData = await Prisma.cargo.findMany()
     return cargosData.length>0? cargosData : null
 }
+
+export const createCargoService = async (cargo:TCargo)=>{
+    const cargoData = await Prisma.cargo.create({
+        data: cargo
+    })
+    return cargoData
+}
+
