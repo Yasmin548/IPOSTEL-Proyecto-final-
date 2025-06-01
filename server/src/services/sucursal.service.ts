73261156eb86b362d47306fcbacf0a6d70602b1d
Moisarm <moisarm03@gmail.com>
2025-06-01 15:52:49 -0400

    [ADD] find sucursal y by id service

--- server/src/services/sucursal.service.ts
+++ server/src/services/sucursal.service.ts
@@ -4,3 +4,10 @@ export const sucursalListService = async ()=>{
     const sucursalData = await Prisma.sucursal.findMany()
     return sucursalData.length>0? sucursalData : null
 }
+
+export const findSucursalByID= async (rif: string)=>{
+    const sucursalData= await Prisma.sucursal.findUnique({
+        where:{rif:rif}
+    })
+    return sucursalData
+}
