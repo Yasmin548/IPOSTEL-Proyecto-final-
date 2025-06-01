6b720bdac513bc69ff148b4afa422702173618c9
Moisarm <moisarm03@gmail.com>
2025-06-01 03:25:55 -0400

    [ADD] documenting code

--- server/src/routes/cargo.route.ts
+++ server/src/routes/cargo.route.ts
@@ -10,31 +10,37 @@ export const cargoRouter = express.Router()
 const controller = new cargoController
 
 cargoRouter
+//Obtiene todos los cargos
 .get('/', async (req:Request, res:Response)=>{
     const response = await controller.cargoListController()
     sendResponse(res, response)
 })
+//Crea un cargo
 .post('/', validateClass(createCargoDTO), async (req:Request, res:Response)=>{
     const cargo : TCargo = req.body
     const response = await controller.createCargoController(cargo)
     sendResponse(res, response)
 })
+//Busca un cargo por id
 .get('/:id', async(req:Request, res:Response)=>{
     const id = Number(req.params.id)
     const response= await controller.searchCargoByIDController(id)
     sendResponse(res, response)
 })
+//Busca un cargo por nombre
 .get('/search/:nombre', async(req:Request, res:Response)=>{
     const nombre = String(req.params.nombre)
     const response = await controller.searchCargoByNameController(nombre)
     sendResponse(res, response)
 })
+//Actualiza un cargo
 .put('/:id', validateClass(updateCargoDTO), async(req:Request, res:Response)=>{
     const id = Number(req.params.id)
     const cargo = req.body
     const response= await controller.updateCargoController(id, cargo)
     sendResponse(res, response)
 })
+//Elimina un cargo
 .delete('/:id', async(req:Request, res:Response)=>{
     const id = Number(req.params.id)
     const response= await controller.deleteCargoController(id)
