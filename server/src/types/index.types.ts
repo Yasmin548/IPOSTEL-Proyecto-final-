01ddbc8c018f3c8f79b169d2b5f696685ecdbd3c
Moisarm <moisarm03@gmail.com>
2025-06-16 03:17:03 -0400

    [Add] Type message

--- server/src/types/index.types.ts
+++ server/src/types/index.types.ts
@@ -1,10 +1,11 @@
-import { Cargo, Empleado, Sucursal, Usuario } from "../../generated/prisma";
+import { Cargo, Empleado, mensaje, Sucursal, Usuario } from "../../generated/prisma";
 
 
 export type TEmpleado = Empleado
 export type TUsuario = Usuario
 export type TSucursal = Sucursal
 export type TCargo = Cargo
+export type TMessage = mensaje
 
 // T es el parámetro que se le pasa a la función, en este caso la data
 export interface IFunctionResponse<T> {
