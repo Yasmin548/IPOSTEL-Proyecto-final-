ba71fc64cecb57f5d124e6588f80f8fafb9f68dc
Moisarm <moisarm03@gmail.com>
2025-07-13 02:35:36 -0400

    [Fix] implemented an interface to clean the readibility of the code

--- server/src/utils/sendResponse.util.ts
+++ server/src/utils/sendResponse.util.ts
@@ -1,10 +1,11 @@
-import {Response} from "express"
+import { Response } from "express";
+interface response<T> {
+  status: number;
+  message: string;
+  data?: T;
+  error?: unknown;
+}
 
-export function sendResponse <T>(res:Response, response:{
-    status: number,
-    message: string,
-    data?:T,
-    error?: unknown
-}){
-    res.status(response.status).json(response)
+export function sendResponse<T>(res: Response, response: response<T>) {
+  res.status(response.status).json(response);
 }
